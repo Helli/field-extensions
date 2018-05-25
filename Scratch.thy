e234c5eecdea0052ebe63a66454ce28e0c3cf35a
Helli <hellauer@in.tum.de>
2018-05-25 15:03:26 +0200

    more qualifiers; another result (which I don't understand yet)

--- Scratch.thy
+++ Scratch.thy
@@ -9,19 +9,19 @@ begin
 
 section\<open>Quick test\<close>
 
-abbreviation standard_ring :: "'a::{one,times,plus,zero} ring" ("\<S>")
+abbreviation standard_ring :: "_::{one,times,plus,zero} ring" ("\<S>")
   where "standard_ring \<equiv> \<lparr>carrier = UNIV, mult = op *, one = 1, zero = 0, add = op +\<rparr>"
 
-lemma \<S>_cring: "cring (\<S>::_::field ring)"
+lemma \<S>_cring: "Ring.cring (\<S>::_::Fields.field ring)"
   by (auto intro!: cringI abelian_groupI comm_monoidI
     left_minus distrib_right)
 
-lemma \<S>_field: "field (\<S>::_::field ring)"
+lemma \<S>_field: "Ring.field (\<S>::_::Fields.field ring)"
   apply (rule cring.cring_fieldI2)
     apply (fact \<S>_cring) apply auto using dvd_field_iff
   by (metis dvdE)
 
-definition rat_field::"rat ring" ("\<rat>") where "\<rat> = \<S>"
+definition rat_field::"rat Ring.ring" ("\<rat>") where "\<rat> = \<S>"
 definition real_field::"real ring" ("\<real>") where "\<real> = \<S>"
 definition complex_field::"complex ring" ("\<complex>") where "\<complex> = \<S>"
 
@@ -29,9 +29,11 @@ lemma "field \<rat>" unfolding rat_field_def by (fact \<S>_field)
 lemma "field \<real>" unfolding real_field_def by (fact \<S>_field)
 lemma "field \<complex>" unfolding complex_field_def by (fact \<S>_field)
 
-lemma R_id_eval:
-  "UP_pre_univ_prop \<real> \<real> id"
-  by (fast intro: UP_pre_univ_propI \<S>_cring id_ring_hom)
+abbreviation \<K>::"_::field ring" where "\<K> \<equiv> \<S>"
+
+lemma \<K>_id_eval:
+  "UP_pre_univ_prop \<K> \<K> id"
+  by (simp add: UP_pre_univ_propI \<S>_cring rat_field_def)
 
 section\<open>Observations\<close>
 
