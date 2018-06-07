4c1f0baf590f9e49fbac97eb7edff48a44bb00cb
Helli <hellauer@in.tum.de>
2018-06-07 15:58:02 +0200

    outsource and extend examples

---
+++ Examples.thy
@@ -0,0 +1,50 @@
+section \<open>Quick Tests\<close>
+theory Examples imports Scratch
+begin
+
+lemma easy: "of_int ` \<int> \<subseteq> \<rat>" by auto
+
+definition standard_ring
+  where "standard_ring A = \<lparr>carrier = A, mult = ( *), one = 1, zero = 0, add = (+)\<rparr>"
+
+definition univ_ring ("\<U>")
+  where "univ_ring \<equiv> \<lparr>carrier = UNIV, mult = ( *) , one = 1, zero = 0, add = (+)\<rparr>"
+
+lemma \<U>_ring: "Ring.ring (\<U>::_::Rings.ring_1 ring)"
+  unfolding univ_ring_def
+  apply (intro ringI abelian_groupI monoidI)
+  apply (auto simp: ring_distribs mult.assoc)
+  using ab_group_add_class.ab_left_minus apply blast
+  done
+
+lemma \<U>_cring: "Ring.cring (\<U>::_::Fields.field ring)"
+  by (auto intro!: cringI abelian_groupI comm_monoidI
+    left_minus distrib_right simp: univ_ring_def)
+
+lemma \<U>_field: "Ring.field (\<U>::_::Fields.field ring)"
+  apply (rule cring.cring_fieldI2)
+    apply (fact \<U>_cring) apply (auto simp: univ_ring_def) using dvd_field_iff
+  by (metis dvdE)
+
+definition rat_field::"rat ring" where "rat_field = \<U>"
+definition real_field::"real ring" where "real_field = \<U>"
+definition complex_field::"complex ring" where "complex_field = \<U>"
+
+lemma "field rat_field" "field real_field" "field complex_field"
+  unfolding rat_field_def real_field_def complex_field_def by (fact \<U>_field)+
+
+abbreviation \<K>::"_::field ring" where "\<K> \<equiv> univ_ring"
+
+lemma \<K>_id_eval:
+  "UP_pre_univ_prop \<K> \<K> id"
+  using UP_pre_univ_propI \<U>_cring id_ring_hom by blast
+
+find_theorems "Ring.ring" name: I
+
+term "\<Z>"
+term INTEG
+lemma "ring.subring (univ_ring :: rat ring) (standard_ring (range rat_of_int))"
+  unfolding ring.subring_def[OF \<U>_ring]
+  apply (auto simp add: univ_ring_def) unfolding standard_ring_def
+
+end
--- Scratch.thy
+++ Scratch.thy
@@ -6,10 +6,6 @@ theory Scratch imports
 "HOL-Number_Theory.Residues"       (* \<int>/p\<int> and all(?) of the above *)
 begin
 
-lemma easy: "of_int ` (\<int>::int set) \<subseteq> (\<rat>:: rat set)" by auto
-
-definition standard_ring
-  where "standard_ring A = \<lparr>carrier = A, mult = ( *), one = 1, zero = 0, add = (+)\<rparr>"
 
 section \<open>Subrings\<close>
 
@@ -54,6 +50,12 @@ lemma subfield_refl: "subfield R"
 
 end
 
+text\<open>\<^theory_text>\<open>
+locale ideal = additive_subgroup I R + ring R for I and R (structure) +
+  assumes I_l_closed: "\<lbrakk>a \<in> I; x \<in> carrier R\<rbrakk> \<Longrightarrow> x \<otimes> a \<in> I"
+    and I_r_closed: "\<lbrakk>a \<in> I; x \<in> carrier R\<rbrakk> \<Longrightarrow> a \<otimes> x \<in> I"
+\<close>\<close>
+
 locale field_extension =
   fixes L (structure)
   fixes K (structure) (* Which one is the implicit one in this case? *)
@@ -68,47 +70,6 @@ end
 lemma f_e_refl: "field K \<Longrightarrow> field_extension K K"
   by (simp add: field.subfield_refl field_extension.intro)
 
-section \<open>quick test\<close>
-
-definition univ_ring ("\<U>")
-  where "univ_ring \<equiv> \<lparr>carrier = UNIV, mult = ( *) , one = 1, zero = 0, add = (+)\<rparr>"
-
-lemma \<U>_cring: "Ring.cring (\<U>::_::Fields.field ring)"
-  by (auto intro!: cringI abelian_groupI comm_monoidI
-    left_minus distrib_right simp: univ_ring_def)
-
-lemma \<U>_field: "Ring.field (\<U>::_::Fields.field ring)"
-  apply (rule cring.cring_fieldI2)
-    apply (fact \<U>_cring) apply (auto simp: univ_ring_def) using dvd_field_iff
-  by (metis dvdE)
-
-definition rat_field::"rat ring" where "rat_field = \<U>"
-definition real_field::"real ring" where "real_field = \<U>"
-definition complex_field::"complex ring" where "complex_field = \<U>"
-
-lemma "field rat_field" "field real_field" "field complex_field"
-  unfolding rat_field_def real_field_def complex_field_def by (fact \<U>_field)+
-
-abbreviation \<K>::"_::field ring" where "\<K> \<equiv> univ_ring"
-
-lemma \<K>_id_eval:
-  "UP_pre_univ_prop \<K> \<K> id"
-  using UP_pre_univ_propI \<U>_cring id_ring_hom by blast
-
-lemma
-  assumes "ring (\<U>::'a::{one,times,plus,zero} ring)"
-  assumes one_in_S: "1 \<in> S"
-  assumes zero_in_S: "0 \<in> S"
-  assumes closed_mult: "\<forall>a\<in>S. \<forall>b\<in>S. a*b \<in> S"
-  assumes closed_plus: "\<forall>a\<in>S. \<forall>b\<in>S. a+b \<in> S"
-  shows "ring (standard_ring (S::'a set))"
-  oops
-
-lemma
-  assumes "cring (\<U>::'a::{one,times,plus,zero} ring)"
-  (*todo*)
-  shows "cring (standard_ring (S::'a set))"
-  oops
 
 section\<open>Observations\<close>
 
