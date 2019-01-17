457c854d58bc12bead4b720e2b8e0aeaadaee5d8
helli <hellauer@in.tum.de>
2019-01-17 23:51:38 +0100

    tuned

--- Doc/Doc.thy
+++ Doc/Doc.thy
@@ -8,11 +8,15 @@ begin
 
 section \<open>Preface\<close>
 (* to-do: make this an actual preface? *)
-text\<open>This work is part of an interdisciplinary project between Mathematics and Computer Science, supervised by Prof.\ Gregor Kemper and Manuel Eberl. The source files are hosted at \<^url>\<open>https://github.com/helli/field-extensions\<close>.\<close>
+text\<open>This work is part of an interdisciplinary project between Mathematics and Computer Science,
+  supervised by Prof.\ Gregor Kemper and Manuel Eberl. The source files are hosted at
+  \<^url>\<open>https://github.com/helli/field-extensions\<close>.\<close>
 
 section \<open>Modelling of Substructures\<close>
 
-text \<open>In Algebra, superstructures generally are defined to be just the inverse of substructures, as is the cases for fields. Thus, modelling the notion of subfield also defines field extensions (which is just another term for superfield).\<close>
+text \<open>In Algebra, superstructures generally are defined to be just the inverse of substructures, as
+  is the cases for fields. Thus, modelling the notion of subfield also defines field extensions
+  (which is just another term for superfield).\<close>
 
 subsection \<open>\<^const>\<open>ring.old_sr\<close>\<close>
 
@@ -20,7 +24,12 @@ subsection \<open>\<^const>\<open>ring.old_sr\<close>\<close>
 
 subsection \<open>\<^const>\<open>subring\<close>\<close>
 
-text \<open>This locale from \ uses the "set+superstructure"-approach, via \<^locale>\<open>subgroup\<close> and \<^locale>\<open>submonoid\<close>. Note however, that \<^locale>\<open>subgroup\<close>'s axioms only describe a technical relation to the superstructure, assumed to be a group. In other words, \<^prop>\<open>subgroup H G \<Longrightarrow> group (G\<lparr>carrier := H\<rparr>)\<close> does not hold without the additional assumption \<^prop>\<open>group G\<close>, equivalently for ring and monoid. It is only under these additional assumptions that these locales coincide with the textbook definitions.\<close>
+text \<open>This locale from \ uses the "set+superstructure"-approach, via \<^locale>\<open>subgroup\<close> and
+  \<^locale>\<open>submonoid\<close>. Note however, that \<^locale>\<open>subgroup\<close>'s axioms only describe a technical
+  relation to the superstructure, assumed to be a group. In other words, \<^prop>\<open>subgroup H G \<Longrightarrow>
+  group (G\<lparr>carrier := H\<rparr>)\<close> does not hold without the additional assumption \<^prop>\<open>group G\<close>,
+  equivalently for ring and monoid. It is only under these additional assumptions that these locales
+  coincide with the textbook definitions.\<close>
 
 section \<open>The locale \<^locale>\<open>field_extension\<close>\<close>
 
