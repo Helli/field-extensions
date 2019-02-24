8da16195ab7b93ed261c619cc5957399068d916e
helli <hellauer@in.tum.de>
2019-02-24 21:32:37 +0100

    tuned

--- Doc/Doc.thy
+++ Doc/Doc.thy
@@ -92,10 +92,10 @@ text \<open>In \<^locale>\<open>UP_field_extension\<close>, within the above-men
  \<^term>\<open>\<alpha>\<close>, Theorem Kemper/16.9b@{cite Algebra1} applies. Its results are distributed:
   \<^item> @{thm[source] irr_exists}, the existence of \<^term>\<open>\<alpha>\<close>'s minimal polynomial "\<^const>\<open>irr\<close>"
   \<^item> @{thm[source] irr_unique}, the uniqueness of \<^const>\<open>irr\<close>
-  \<^item> @{thm[source] irr_irreducible_polynomial}, the irreducibily of \<^const>\<open>irr\<close> in the ring
+  \<^item> @{thm[source] irr_irreducible_polynomial}, the irreducibility of \<^const>\<open>irr\<close> in the ring
    \<^term>\<open>P\<close> of polynomials over \<^term>\<open>K\<close>
-  \<^item> @{thm[source] the_elem_ring_iso_Quot_irr_generate_field}, the isomorphism of its residue class
-   ring with \<open>K(\<alpha>)\<close>
+  \<^item> @{thm[source] the_elem_ring_iso_Quot_irr_generate_field}, the isomorphism of \<^term>\<open>irr\<close>'s
+ residue class ring with \<open>K(\<alpha>)\<close>
 
 All of these are on their own useful for a library, so splitting up the theorem seemed appropriate.\<close>
 (*<*)end(*>*)
